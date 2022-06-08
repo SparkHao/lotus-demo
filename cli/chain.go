2dd1f7629bc68c11f3f43f5dc9c8454b93a54cdc
hss <haoshanshan@tianrukj.com>
2022-06-08 10:59:47 +0800

    update

--- cli/chain.go
+++ cli/chain.go
@@ -1894,7 +1894,7 @@ var ChainInvoke2Cmd = &cli.Command{
 			if msg.Method == 52 || msg.Method == 51 || msg.Method == 16 || msg.Method == 15 || msg.Method == 102 || msg.Method == 103 || msg.Method == 104 ||
 				msg.Method == 71 || msg.Method == 72 || msg.Method == 73 || msg.Method == 74 || msg.Method == 61 || msg.Method == 62 || msg.Method == 63 ||
 				msg.Method == 64 || msg.Method == 32 || msg.Method == 91 || msg.Method == 55 || msg.Method == 7 || msg.Method == 8 || msg.Method == 54 ||
-				msg.Method == 41 {
+				msg.Method == 41 || msg.Method == 17 {
 				afmt.Println(string(wait.Receipt.Return), " ; gas_used: ", wait.Receipt.GasUsed)
 			}
 
